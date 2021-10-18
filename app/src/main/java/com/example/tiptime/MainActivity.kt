f372fc0488b4b2d7ac1e9879b66a7c0556ba7c65
nicko <37054216+nicko-lee@users.noreply.github.com>
2021-10-18 18:43:37 +1100

    Handle some edge cases of user interaction

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -7,7 +7,7 @@ import java.text.NumberFormat
 import kotlin.math.ceil
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,11 +19,14 @@ class MainActivity : AppCompatActivity() {
     private fun calculateTip() {
         // Get the user entered cost
         val stringInTextField = binding.costOfService.text.toString()
-        val cost = stringInTextField.toDouble()
+        val cost = stringInTextField.toDoubleOrNull()
+        if (cost == null) {
+            binding.tipResult.text = ""
+            return
+        }
 
         // Get the tip percentage
-        val selectedId = binding.tipOptions.checkedRadioButtonId
-        val tipPercentage = when (selectedId) {
+        val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
             R.id.option_twenty_percent -> 0.20
             R.id.option_eighteen_percent -> 0.18
             else -> 0.15
@@ -31,8 +34,7 @@ class MainActivity : AppCompatActivity() {
 
         // Calculate the tip and round it up if user toggled Round Up option
         var tip = cost * tipPercentage
-        val roundUp = binding.roundUpSwitch.isChecked
-        if (roundUp) {
+        if (binding.roundUpSwitch.isChecked) {
             tip = ceil(tip)
         }
 
